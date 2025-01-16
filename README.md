0a0d583fae4886a0ebb5425ab654d45e7b4bfafc
YongjinJeon <54887220+lemontrr@users.noreply.github.com>
2025-01-16 10:50:51 +0900

    ches2025 artifact#7 final version
    ches2025 artifact#7 final version

---
+++ README.md
@@ -0,0 +1,92 @@
+# Extended-BP-Framework
+This paper provides guidance on using a tool for optimizing S-box circuits while preserving nonlinear gates and AND-depth.
+
+## Options of main.py
+**-h** : Show the help message and exit.
+**search** : Search for optimized circuits. (default)
+**analyze** : Generates files by analyzing the result and log files.
+
+##### Search Options
+**-f** : Specifies the circuit filename to optimize (excluding the **.py** extension). [default : **AES10**]
+**-m** : Indicates the number of cores for multi-threading. Each core runs independently to optimize the target circuit. If set to 1, a single core is used. [default : 1]
+**-a** : Selects the BP-based algorithm to apply (**RNBP** or **BPD**). [default : **RNBP**]
+**-d** or **-H** : Sets the depth limit. [default : 23]
+**-r** : Enable or disables random circuit modification mode (```True``` or ```False```). [default : ```False```]
+
+## Usage Examples
+This section provides example commands along with explanations of their outcomes.
+
+>python main.py search
+
+This command performs the optimization process for the circuit specified in 
+**code_target_imps/AES10.py**, utilizing eRNBP.
+
+>python main.py -f AES12
+
+The optimization process targets the circuit in **code_target_imps/AES12.py**, utilizing eRNBP.
+
+>python main.py -f AES12 -m 12 -a BPD -H 15
+
+Here, the optimization focuses on the circuit in **code_target_imps/AES12.py**, using eBPD with 12 cores and a depth limit of 15.
+
+>python main.py -f Ascon
+
+In this case, the circuit in **code_target_imps/Ascon.py** is optimized with eRNBP.
+
+>python main.py -f Ascon -m 4 -a BPD -H 5 -r True
+
+This command applies optimization to the circuit in **code_target_imps/Ascon.py** using eBPD with 4 cores, a depth limit of 5, and random circuit modification mode enabled.
+
+>python main.py -f Saturnin -m 8 -a RNBP
+
+The optimization is applied to the circuit in **code_target_imps/Saturnin.py**, utilizing eRNBP on 8 cores.
+
+>python main.py analyze
+
+The analysis processes all results stored in the **code_results** directory, generating a summary in **code_results/CODE_RESULTS.txt**. Log files in the **log** directory are reviewed, with changes in Dist saved to **log/view_Dist** and additional information stored in **log/view_all_Info**.
+
+## Included Circuit Files
+**code_target_imps/AES10.py** : AES S-box circuit in [BP10]
+**code_target_imps/AES12.py** : AES S-box circuit in [BP12]
+**code_target_imps/AES10v2.py** and **code_target_imps/AES10v3.py** : AES S-box circuits made in Section 4.1 based on [BP10]
+**code_target_imps/AES12v2.py** and **code_target_imps/AES12v3.py** : AES S-box circuits made in Section 4.1 based on [BP12]
+**code_target_imps/Ascon.py** : Ascon S-box circuit in [DEMS21]
+**code_target_imps/Saturnin.py** : Saturnin super S-box circuit in [CDL+20]
+**code_target_imps/SNOW_up.py** : Front part of SNOW3G S-box in [BHNS10]
+**code_target_imps/SNOW_dn.py** : Back part of SNOW3G S-box in [BHNS10]
+**code_formal/AES10.py** : Formal type (XOR information) of **code_target_imps/AES10.py**
+Files in **code_results** directory : Results included in the paper.
+
+## How to Upload Circuits
+To optimize a circuit, it must be uploaded as a Python file in the **code_target_imps** directory.
+The implementation of the circuit must be inserted at the position marked by the following comment in the file:
+
+```################### Here is your code !! ###################```
+
+The input and output variables must be listed as ```x``` and ```y```, respectively.
+For further instructions, refer to the example file provided in the **code_target_imps** directory.
+
+## Generated Files by Our Tool
+The **code_results** directory includes the result **.py** files.
+The **code_formal** directory includes temporary **.py** files to extract XOR information.
+The **log** directory includes log files recording the optimization process.
+The **log/view_Dist** directory includes log files that record changes to *Dist*.
+The **log/view_all_Info** directory includes log files containing information other than *Dist*.
+
+## Notes on Analyzing the Results
+Using the **analyze** option, in **code_results/CODE_RESULTS.txt**, the following metrics are analyzed: depth (with NOTs), depth (without NOTs), AND-depth, ANDs, ORs, XORs, and NOTs.
+When all NOT gates are combined into an XNOR gate, the latency of the circuit follows *depth (without NOTs)* rather than *depth (with NOTs)*.
+
+## Running Time
+Our multi-threading option does not improve the speed of one search, but it enables multiple searches to run concurrently. The time taken for one search for each circuit is as follows:
+Ascon S-box : Less than 1s
+AES S-box : 3h - 20h
+A part of SNOW3G S-box : 12h - a day
+Saturnin super S-box : About a week
+
+## References
+[BP10] Joan Boyar and René Peralta. A new combinational logic minimization technique with applications to cryptology. In Paola Festa, editor, Experimental Algorithms, 9th International Symposium, SEA 2010, Ischia Island, Naples, Italy, May 20-22, 2010. Proceedings, volume 6049 of Lecture Notes in Computer Science, pages 178–189. Springer, 2010.
+[BHNS10] Billy Bob Brumley, Risto M. Hakala, Kaisa Nyberg, and Sampo Sovio. Consecutive s-box lookups: A timing attack on SNOW 3g. In Miguel Soriano, Sihan Qing, and Javier López, editors, Information and Communications Security - 12th International Conference, ICICS 2010, Barcelona, Spain, December 15-17, 2010. Proceedings, volume 6476 of Lecture Notes in Computer Science, pages 171–185. Springer, 2010.
+[BP12] Joan Boyar and René Peralta. A small depth-16 circuit for the AES s-box. In Dimitris Gritzalis, Steven Furnell, and Marianthi Theoharidou, editors, Information Security and Privacy Research - 27th IFIP TC 11 Information Security and Privacy Conference, SEC 2012, Heraklion, Crete, Greece, June 4-6, 2012. Proceedings, volume 376 of IFIP Advances in Information and Communication Technology, pages 287–298. Springer, 2012.
+[CDL+20] Anne Canteaut, Sébastien Duval, Gaëtan Leurent, María Naya-Plasencia, Léo Perrin, Thomas Pornin, and André Schrottenloher. Saturnin: a suite of lightweight symmetric algorithms for post-quantum security. IACR Transactions on Symmetric Cryptology, 2020(S1):160–207, 2020.
+[DEMS21] Christoph Dobraunig, Maria Eichlseder, Florian Mendel, and Martin Schläffer. Ascon v1. 2: Lightweight authenticated encryption and hashing. Journal of Cryptology, 34:1–42, 2021.
---
+++ main.py
@@ -0,0 +1,220 @@
+from multiprocessing import Pool
+import multiprocessing as mp
+import Change_Circuit_Formal
+import Extract_XOR_information
+import Modification_Circuits
+import Optimizer_BPD
+import Optimizer_RNBP
+import Write_Circuit
+import Code_spac_analysis
+import time
+import argparse
+import log_reader
+import os
+import sys
+
+
+def Read_sizes(filename):
+    with open(filename, 'r') as fp:
+        lines = fp.read().split(
+            '################### Here is your code !! ###################')[1].split('\n')
+    all_Vars = set()
+    for line in lines:
+        line = line.replace(' ', '').replace(';', '')
+        if '#' in line[:3]:
+            continue
+        if '=' not in line:
+            continue
+        if line.count('=') > 1:
+            continue
+        # print(line)
+        C, AB = line.split('=')
+        all_Vars.add(C.replace('^', '').replace('&', '').replace('|', ''))
+        if '&' in AB:
+            all_Vars.update(AB.split('&'))
+        elif '|' in AB:
+            all_Vars.update(AB.split('|'))
+        elif '^' in AB:
+            all_Vars.update(AB.split('^'))
+    n = 0
+    for i in range(100, -1, -1):
+        if f'x[{i}]' in all_Vars:
+            n = i+1
+            break
+    m = 0
+    for i in range(100, -1, -1):
+        if f'y[{i}]' in all_Vars:
+            m = i+1
+            break
+    return n, m
+
+
+def optimizer_with_BPD(n, m, XORs, NLs, NOTs, filename, pc_name, H):
+    st = time.time()
+    add_on_logname = f'_{H}H_{pc_name}'
+    circuit, XORnum = Optimizer_BPD.Optimizer_with_BPD(
+        n, m, XORs, NLs, NOTs, log_filename=filename + add_on_logname, H=H)
+
+    add_on_name = f'_{H}H_{XORnum}XORs_{pc_name}_{int(time.time()-st)}s'
+    Write_Circuit.Write_Circuit(filename, add_on_name, circuit)
+    print(f'Write {filename} (H = {H}) by using {XORnum}XORs in {
+          pc_name} (time : {time.time()-st:.2f}s)')
+    return 1
+
+
+def optimizer_with_BPD_for_modified_circuit(n, m, XORs, NLs, NOTs, filename, pc_name, H):
+    st = time.time()
+    new_XORs, new_NLs, new_NOTs, _ = Modification_Circuits.Modify_circuits(
+        n, m, XORs, NLs, NOTs)
+
+    add_on_logname = f'_{H}H_{pc_name}'
+    circuit, XORnum = Optimizer_BPD.Optimizer_with_BPD(
+        n, m, new_XORs, new_NLs, new_NOTs, log_filename=filename + add_on_logname, H=H)
+
+    add_on_name = f'_{H}H_{XORnum}XORs_{pc_name}_{int(time.time()-st)}s'
+    Write_Circuit.Write_Circuit(filename, add_on_name, circuit)
+    print(f'Write {filename} (H = {H}) by using {XORnum}XORs in {
+          pc_name} (time : {time.time()-st:.2f}s)')
+    return 1
+
+
+def optimizer_with_RNBP(n, m, XORs, NLs, NOTs, filename, pc_name):
+    st = time.time()
+    add_on_logname = f'_RNBP_{pc_name}'
+    circuit, XORnum = Optimizer_RNBP.Optimizer_with_RNBP(
+        n, m, XORs, NLs, NOTs, log_filename=filename + add_on_logname)
+
+    add_on_name = f'_RNBP_{XORnum}XORs_{pc_name}_{int(time.time()-st)}s'
+    Write_Circuit.Write_Circuit(filename, add_on_name, circuit)
+    print(f'Write {filename} by using {XORnum}XORs in {
+          pc_name} (time : {time.time()-st:.2f}s)')
+    return 1
+
+
+def optimizer_with_RNBP_for_modified_circuit(n, m, XORs, NLs, NOTs, filename, pc_name):
+    st = time.time()
+    new_XORs, new_NLs, new_NOTs, _ = Modification_Circuits.Modify_circuits(
+        n, m, XORs, NLs, NOTs)
+
+    add_on_logname = f'_RNBP_{pc_name}'
+    circuit, XORnum = Optimizer_RNBP.Optimizer_with_RNBP(
+        n, m, new_XORs, new_NLs, new_NOTs, log_filename=filename + add_on_logname)
+
+    add_on_name = f'_RNBP_{XORnum}XORs_{pc_name}_{int(time.time()-st)}s'
+    Write_Circuit.Write_Circuit(filename, add_on_name, circuit)
+    print(f'Write {filename} by using {XORnum}XORs in {
+          pc_name} (time : {time.time()-st:.2f}s)')
+    return 1
+
+
+if __name__ == '__main__':
+    descript = "This is the tool to optimize S-box circuit."
+    # descript += "The main paper is 'A Framework for Generating S-Box Circuits with Boyer-Peralta Algorithm-Based Heuristics, and Its Applications to AES and Saturnin'\n"
+
+    parser = argparse.ArgumentParser(description=descript)
+
+    parser.add_argument(
+        "-f", "--filename", help="Circuit to optimize (default 'AES10')", type=str, default='AES10')
+    parser.add_argument(
+        "-m", "--multi", help="The number of cores for multi threading (default 1)", type=int, default=1)
+    parser.add_argument(
+        "-a", "--algorithm", help="BP based algorithm to be incorporated (default 'RNBP')", type=str, default='RNBP')
+    parser.add_argument("-H", "-d", "--depth_limit",
+                        help="The depth limit (default : 23)", type=int, default=23)
+    parser.add_argument(
+        "-r", "--random", help="Random circuit modification mode (default : False)", type=bool, default=False)
+
+    subparser = parser.add_subparsers(
+        dest="command", help="Available commands")
+    srch_parser = subparser.add_parser(
+        "search", help="Search an S-box circuit (default)")
+    anal_parser = subparser.add_parser("analyze", help="Analyze the data")
+
+    args = parser.parse_args()
+
+    if len(sys.argv) == 1:
+        parser.print_help()
+
+    elif args.command == "analyze":
+        filenames = os.listdir('./code_results')
+        text = ''
+        for filename in filenames:
+            if filename == 'CODE_RESULTS.txt':
+                continue
+            n, m = Read_sizes('./code_results/'+filename)
+            DN, D, AD, ANDs, ORs, XORs, NOTs = Code_spac_analysis.check_implementation_spec_in_results(
+                filename, n, m)
+            text += f'Performance of {filename[:-3]} ::: depth(with NOTs) : {DN} / depth : {D} / AND-depth : {AD} / ANDs : {ANDs} / ORs : {ORs} / XORs : {XORs} / NOTs : {NOTs}\n'
+        with open('./code_results/CODE_RESULTS.txt', 'w') as fp:
+            fp.write(text)
+
+        if not os.path.exists('./log/view_Dist'):
+            os.makedirs('./log/view_Dist')
+        if not os.path.exists('./log/view_all_Info'):
+            os.makedirs('./log/view_all_Info')
+        filenames = os.listdir('./log')
+        for filename in filenames:
+            if '.txt' not in filename:
+                continue
+            text = log_reader.anal_Dist(filename[:-4])
+            with open('./log/view_Dist/'+filename[:-4]+'.txt', 'w') as fp:
+                fp.write(text)
+            text = log_reader.anal_Informations(filename[:-4])
+            with open('./log/view_all_Info/'+filename[:-4]+'.txt', 'w') as fp:
+                fp.write(text)
+
+    else:
+        n, m = Read_sizes('./code_target_imps/'+args.filename+'.py')
+        if args.multi == 1:
+            multi_proc = 'a single core'
+        else:
+            multi_proc = f'{args.multi} multi cores'
+        if args.algorithm == 'RNBP':
+            algorithm = 'RNBP'
+        elif args.algorithm == 'BPD':
+            algorithm = f'BPD (H = {args.depth_limit})'
+        if args.random == True:
+            modify = ' with randomly modificatons'
+        elif args.random == False:
+            modify = ''
+        print(f'I will optimize {args.filename} ({
+              n}-bit -> {m}-bit) on {multi_proc} using {algorithm}' + modify)
+
+        Change_Circuit_Formal.circuit_formal(n, m, args.filename)
+        XORs, NLs, NOTs = Extract_XOR_information.extract_XOR_NOTs(
+            n, m, args.filename)
+        if args.multi == 1:
+            if (args.algorithm == 'RNBP') and (args.random == False):
+                optimizer_with_RNBP(n, m, XORs, NLs, NOTs,
+                                    args.filename, 'single')
+            elif (args.algorithm == 'RNBP') and (args.random == True):
+                optimizer_with_RNBP_for_modified_circuit(
+                    n, m, XORs, NLs, NOTs, args.filename, 'single')
+            elif (args.algorithm == 'BPD') and (args.random == False):
+                optimizer_with_BPD(n, m, XORs, NLs, NOTs,
+                                   args.filename, 'single', args.depth_limit)
+            elif (args.algorithm == 'BPD') and (args.random == True):
+                optimizer_with_BPD_for_modified_circuit(
+                    n, m, XORs, NLs, NOTs, args.filename, 'single', args.depth_limit)
+        elif args.multi > 1:
+            p = Pool(args.multi)
+            ret = [0]*args.multi
+            if (args.algorithm == 'RNBP') and (args.random == False):
+                for pc_cnt in range(args.multi):
+                    ret[pc_cnt] = p.apply_async(optimizer_with_RNBP, [
+                                                n, m, XORs, NLs, NOTs, args.filename, f'core{pc_cnt:02d}'])
+            elif (args.algorithm == 'RNBP') and (args.random == True):
+                for pc_cnt in range(args.multi):
+                    ret[pc_cnt] = p.apply_async(optimizer_with_RNBP_for_modified_circuit, [
+                                                n, m, XORs, NLs, NOTs, args.filename, f'core{pc_cnt:02d}'])
+            elif (args.algorithm == 'BPD') and (args.random == False):
+                for pc_cnt in range(args.multi):
+                    ret[pc_cnt] = p.apply_async(optimizer_with_BPD, [
+                                                n, m, XORs, NLs, NOTs, args.filename, f'core{pc_cnt:02d}', args.depth_limit])
+            elif (args.algorithm == 'BPD') and (args.random == True):
+                for pc_cnt in range(args.multi):
+                    ret[pc_cnt] = p.apply_async(optimizer_with_BPD_for_modified_circuit, [
+                                                n, m, XORs, NLs, NOTs, args.filename, f'core{pc_cnt:02d}', args.depth_limit])
+            [r.get() for r in ret]
+            p.close()
+            p.join()
